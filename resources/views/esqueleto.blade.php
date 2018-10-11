796aac91e7170e2ca4ed66c333285740ccbad139
Esdras L. Bispo Jr <esdraspiano@gmail.com>
2018-10-11 19:08:51 -0300

    Inclusão do Logo na página principal.

--- resources/views/esqueleto.blade.php
+++ resources/views/esqueleto.blade.php
@@ -61,11 +61,18 @@
     </nav>
 
     <main role="main" class="container">
-      <div class="jumbotron">
-        <h1>Jataí ACM SIGCSE Chapter</h1>
-        <p class="lead">A referência local da ACM em Educação de Computação em Jataí, Goiás.</p>
-        <a class="btn btn-lg btn-primary" href="../../components/navbar/" role="button">Conheça mais sobre o nosso capítulo &raquo;</a>
-      </div>
+        <div class="jumbotron">
+            <div class="row">
+                <div class="col-md-2">
+                    <img src="/img/logo-chapter-jatai.png" class="img-fluid" alt="Responsive image">
+                </div>
+                <div class="col-md-10">
+                    <h1>Jataí ACM SIGCSE Chapter</h1>
+                    <p class="lead">A referência local da ACM em Educação de Computação em Jataí, Goiás.</p>
+                    <a class="btn btn-lg btn-primary" href="../../components/navbar/" role="button">Conheça mais sobre o nosso capítulo &raquo;</a>
+                </div>
+            </div>
+        </div>
     </main>
 
     <!-- Bootstrap core JavaScript
