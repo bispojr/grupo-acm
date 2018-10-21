cd5e01aba9292accfd89e5d6a943274f56dac940
Esdras L. Bispo Jr <esdraspiano@gmail.com>
2018-10-20 23:23:54 -0300

    Conclusão dos testes da NavBar.
    Closes #33

--- tests/Browser/NavbarTest.php
+++ tests/Browser/NavbarTest.php
@@ -29,4 +29,54 @@ class NavbarTest extends DuskTestCase
                     ->assertPathIs('/sobre');
         });
     }
+    public function testMembros()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->clicklink("Membros")
+                    ->assertPathIs('/membros');
+        });
+    }
+    public function testAgenda()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->clicklink("Agenda")
+                    ->assertPathIs('/agenda');
+        });
+    }
+    public function testCertificadosBuscar()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->clicklink("Certificados")
+                    ->clickLink("Buscar")
+                    ->assertPathIs('/certificados/buscar');
+        });
+    }
+    public function testCertificadosValidar()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->clicklink("Certificados")
+                    ->clickLink("Validar")
+                    ->assertPathIs('/certificados/validar');
+        });
+    }
+    public function testRecursos()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->clicklink("Recursos")
+                    ->assertPathIs('/recursos');
+        });
+    }
+    public function testFaleConosco()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->clicklink("Fale Conosco")
+                    ->assertPathIs('/fale-conosco');
+        });
+    }
 }
