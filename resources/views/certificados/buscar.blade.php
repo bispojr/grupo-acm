98ae97b2fabecbebb457b68a0d58ebdf0f0f2079
Esdras L. Bispo Jr <esdraspiano@gmail.com>
2018-10-24 09:55:55 -0300

    Criação das views Buscar e Validar.

--- resources/views/certificados/buscar.blade.php
+++ resources/views/certificados/buscar.blade.php
@@ -4,9 +4,14 @@
     <div class="col-md-10">
         <h1>Buscar Certificados</h1>
 
-        <p class="lead" style="margin-top:1em;">
-            Mussum Ipsum, cacilds vidis litro abertis. Delegadis gente finis, bibendum egestas augue arcu ut est. Nec orci ornare consequat. Praesent lacinia ultrices consectetur. Sed non ipsum felis. Copo furadis é disculpa de bebadis, arcu quam euismod magna. Pra lá , depois divoltis porris, paradis. 
-        </p>
+        <form method="POST" class="form-inline" style="margin-top:1em;" action='{{ url('/certificados/buscar') }}'>
+            @csrf
+            <div class="form-group mx-sm-3 mb-2">
+                <label for="cpf" class="sr-only">CPF</label>
+                <input type="text" class="form-control" id="cpf" placeholder="Digite o seu CPF.">
+            </div>
+            <button type="submit" class="btn btn-primary mb-2">Buscar</button>
+        </form>
     </div>
     <div class="col-md-1"></div>
   </div>
